829b5a916aded235185c9b07b355cfc522bc1a58
Roland Yonaba <roland.yonaba@gmail.com>
2013-03-31 13:17:07 +0100

    Pushed 4-connected variant

---
+++ floodfill/flood4.lua
@@ -0,0 +1,12 @@
+local function flood4(x, y, grid)
+	local actualValue = grid:get(x,y)
+	if actualValue and (actualValue == 0) then
+		grid:set(x,y,1)
+		flood4(x+1,y,grid)
+		flood4(x-1,y,grid)
+		flood4(x,y+1,grid)
+		flood4(x,y-1,grid)
+	end
+end
+
+return flood4
