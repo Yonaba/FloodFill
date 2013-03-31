cb74b010860aa200231133fea84e448a28e7f824
Roland Yonaba <roland.yonaba@gmail.com>
2013-03-31 20:10:41 +0100

    Prevent from endless loops

--- floodfill/flood8stack.lua
+++ floodfill/flood8stack.lua
@@ -1,11 +1,12 @@
-local function flood8stack(x, y, stack, grid)
+local function flood8stack(x, y, grid, stack)
 	stack:clear()
 	if grid:isLegit(x,y) then stack:push({x,y}) end
 	while not stack:isEmpty() do
 		local p = stack:pop()
 		local actualValue = grid:get(p[1], p[2])
 		if actualValue and (actualValue == 0) then
-			grid:set(p[1],p[2],1)
+			x, y = p[1], p[2]
+			grid:set(x, y, 1)
 			if grid:isLegit(x+1, y) and grid:get(x+1,y)==0 then stack:push({x+1,y}) end
 			if grid:isLegit(x-1, y) and grid:get(x-1,y)==0 then stack:push({x-1,y}) end
 			if grid:isLegit(x, y+1) and grid:get(x,y+1)==0 then stack:push({x,y+1}) end
