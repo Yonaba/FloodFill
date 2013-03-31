7dc6ec61b4c7a1b093f8f1e7ef77944eceef03d1
Roland Yonaba <roland.yonaba@gmail.com>
2013-03-31 22:50:53 +0100

    Removed unnecessary checks

--- floodfill/flood4stack.lua
+++ floodfill/flood4stack.lua
@@ -6,13 +6,13 @@ local function flood4stack(x, y, grid, stack)
 	while not stack:isEmpty() do
 		local p = stack:pop()	
 		local actualValue = grid:get(p[1], p[2])
-		if actualValue and (actualValue == 0) then
+		if (actualValue == 0) then
 			x, y = p[1], p[2]
 			grid:set(x, y, 1)
-			if grid:isLegit(x+1, y) and grid:get(x+1,y)==0 then stack:push({x+1,y}) end
-			if grid:isLegit(x-1, y) and grid:get(x-1,y)==0 then stack:push({x-1,y}) end
-			if grid:isLegit(x, y+1) and grid:get(x,y+1)==0 then stack:push({x,y+1}) end
-			if grid:isLegit(x, y-1) and grid:get(x,y-1)==0 then stack:push({x,y-1}) end
+			if grid:isLegit(x+1, y) then stack:push({x+1,y}) end
+			if grid:isLegit(x-1, y) then stack:push({x-1,y}) end
+			if grid:isLegit(x, y+1) then stack:push({x,y+1}) end
+			if grid:isLegit(x, y-1) then stack:push({x,y-1}) end
 		end
 	end
 end
