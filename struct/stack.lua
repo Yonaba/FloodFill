5092e6a1be853ae62a0073ebbb6ffa58cd030a8f
Roland Yonaba <roland.yonaba@gmail.com>
2013-04-01 11:34:28 +0100

    Keeps tracks of pushed items, to prevent from duplicates

--- struct/stack.lua
+++ struct/stack.lua
@@ -2,7 +2,7 @@
 
 local t_remove = table.remove
 
-local stack = {_stack={}, _pointer = 0}
+local stack = {_stack={}, _pointer = 0, _has = {}}
 stack.__index = stack
 
 function stack:new()
@@ -10,8 +10,10 @@ function stack:new()
 end
 
 function stack:push(item)
+	if self._has[item] then return end
 	self._pointer = self._pointer+1
 	self._stack[self._pointer] = item
+	self._has[item] = true
 	return self
 end
 
@@ -19,6 +21,7 @@ function stack:pop()
 	local item = self._stack[self._pointer]
 	self._pointer = self._pointer-1
 	t_remove(self._stack)
+	self._has[item] = nil
 	return item
 end
 
@@ -28,6 +31,7 @@ end
 
 function stack:clear()
 	self._stack = {}
+	self._has = {}
 	self._pointer = 0
 	return self
 end
