1c9d0c75854eb018fb7b294fd6e22a6b33b52d3b
Roland Yonaba <roland.yonaba@gmail.com>
2013-04-01 11:33:31 +0100

    Uses hardcoded nodes, to avoid reallocating tables Renamed isFilled to isFlooded Rename isLegit to has

--- struct/grid.lua
+++ struct/grid.lua
@@ -7,28 +7,28 @@ function grid:new(width, height)
 	local g = setmetatable({},grid)
 	g._width, g._height = width, height
 	for y = 1, g._height do g._map[y] = {}
-		for x = 1, g._width do g._map[y][x] = 0 end
+		for x = 1, g._width do g._map[y][x] = {x = x, y = y, v = 0} end
 	end
 	return g
 end
 
-function grid:isLegit(x,y)
-	return (x>0 and x<=self._width) and (y>0 and y<=self._height)
+function grid:has(x,y)
+	return self._map[y] and self._map[y][x]
 end
 
 function grid:set(x,y,value)
-	self._map[y][x] = value
+	self._map[y][x].v = value
 	return self
 end
 
 function grid:get(x,y)
-	return self._map[y] and self._map[y][x]
+	return self._map[y][x].v
 end
 
-function grid:isFilled()
+function grid:isFlooded()
 	for y = 1, self._width do
 		for x = 1, self._height do
-			if self._map[y][x] == 0 then return false end
+			if self._map[y][x].v == 0 then return false end
 		end
 	end
 	return true
@@ -36,7 +36,7 @@ end
 
 function grid:reset()
 	for y = 1, self._height do
-		for x = 1, self._width do self._map[y][x] = 0 end
+		for x = 1, self._width do self._map[y][x].v = 0 end
 	end
 	return self
 end
