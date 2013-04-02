79b9391f1eb7d59c8bcbf510dc4a5dc2c3ea8e8a
Roland Yonaba <roland.yonaba@gmail.com>
2013-04-02 18:48:22 +0100

    Expanded opts

--- utils/getopt.lua
+++ utils/getopt.lua
@@ -3,6 +3,25 @@ local fExists = function(file)
 	return (f~=nil)
 end
 
+local keys = function(t)
+	local k = {}
+	local has = false
+	for _,v in pairs(t) do
+		has = true
+		k[#k+1] = v
+	end
+	return has and k or false
+end
+
+local fSetExists = function(v)
+	for alg in v:gmatch('%.*(%w+)%.*') do
+		if not fExists(('floodfill/%s.lua'):format(alg):gsub('queue.lua$', 'stack.lua')) then
+			return false
+		end
+	end
+	return true
+end
+
 local extend = function(dest, source)
 	for k,v in pairs(source) do
 		if not dest[k] then dest[k] = v.value end
@@ -11,24 +30,32 @@ local extend = function(dest, source)
 end
 
 local defArgs = {
-	['repeat'] = {value = 10, assert = function(v)
+	['r'] = {value = 10, assert = function(v)
 		local v = tonumber(v)
 		return v and v>0
 	end},
-	['map'] = {value = 'orz203d', assert = function(v)
+	['m'] = {value = 'orz203d', assert = function(v)
 		 v = not v:find('%.map$') and v .. '.map' or v
 		return fExists(('maps/%s'):format(v))
+	end},
+	['i'] = {value = '', assert = function(v)
+		return fSetExists(v)
+	end},
+	['u'] = {value = '', assert = function(v)
+		return fSetExists(v)
 	end}
 }
 
 local function getOpt(str)
 	str = str or ''
 	local args = {}
-	for optArg, value in str:gmatch('%-(%w+)=(.+)%s*') do
+	print('str',str)
+	for optArg, value in str:gmatch('%-(%w+)%s*=%s*([%_%-%.%w+]+)') do
+		print(optArg,value)
 		if defArgs[optArg] then
 			if value then
 				assert(defArgs[optArg].assert(value),
-					('Wrong value to option -%s'):format(optArg))
+					('Wrong value to option -%s (%s)'):format(optArg, value))
 			end
 			args[optArg] = value
 		else
@@ -37,3 +64,5 @@ local function getOpt(str)
 	end
 	return extend(args, defArgs)
 end
+
+return getOpt
